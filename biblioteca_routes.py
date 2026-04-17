7a2044ad1a2d188868b16dd2ce0395ce3f367bd8
saulo-npereira <saulopereira1811@gmail.com>
2026-04-16 23:41:21 -0300

    adicionando sistema de listagem de emprestimos e emprestimos atrasados, e sistema de limite de emprestimo

--- biblioteca_routes.py
+++ biblioteca_routes.py
@@ -1,11 +1,29 @@
 from fastapi import APIRouter, Depends, HTTPException
 from schemas import LivroSchema
 from sqlalchemy.orm import Session
+from sqlalchemy import or_, and_
 from dependencies import pegar_sessao, verificar_admin, verificar_token
 from models import Livros, Usuarios, Emprestimos
 from datetime import datetime, timedelta
 
 biblioteca_router = APIRouter(prefix='/bibliotecas', tags=['biblioteca'])
+
+def formatar_emprestimo(e):
+    return {
+        "id": e.id,
+        "livro_id": e.id_livro,
+        "usuario_id": e.id_usuario,
+        "status": e.status,
+        "data_emprestimo": e.data_emprestimo.strftime("%d/%m/%Y %H:%M"),
+        "data_devolucao_prevista": e.data_devolucao_prevista.strftime("%d/%m/%Y %H:%M") if e.data_devolucao_prevista else None,
+        "data_devolucao_real": e.data_devolucao_real.strftime("%d/%m/%Y %H:%M") if e.data_devolucao_real else None
+    }
+
+def detectar_atraso(data_prevista, data_agora = datetime.utcnow()):
+    if data_prevista < data_agora:
+        return True
+    return False
+
 
 
 @biblioteca_router.post('/adicionar_livro')
@@ -84,6 +102,9 @@ async def buscar_livro_por_titulo(titulo_livro: str, session: Session = Depends(
 
 @biblioteca_router.post('/pegar_emprestado/{id_livro}')
 async def pegar_emprestado(id_livro: int, session: Session = Depends(pegar_sessao), usuario: Usuarios = Depends(verificar_token)):
+    usuario_emprestimos_ativos = session.query(Emprestimos).filter(and_(Emprestimos.id_usuario==usuario.id, or_(Emprestimos.status=='ativo', Emprestimos.status=='atrasado'))).count()
+    if usuario_emprestimos_ativos >= 3:
+        raise HTTPException(status_code=400, detail='Você já possui 3 emprestimos não devolvidos')
     emprestimo_existe = session.query(Emprestimos).filter(
         Emprestimos.id_usuario==usuario.id,
         Emprestimos.id_livro==id_livro,
@@ -114,7 +135,7 @@ async def devolver_livro(id_livro: int, usuario: Usuarios = Depends(verificar_to
     emprestimo = session.query(Emprestimos).filter(
         Emprestimos.id_livro == id_livro,
         Emprestimos.id_usuario == usuario.id,
-        Emprestimos.status == 'ativo'
+        or_(Emprestimos.status == 'ativo', Emprestimos.status == 'atrasado')
     ).first()
     if not emprestimo:
         raise HTTPException(status_code=400, detail='Você ainda não pegou esse livro emprestado')
@@ -127,3 +148,62 @@ async def devolver_livro(id_livro: int, usuario: Usuarios = Depends(verificar_to
     return {
         'message': f'usuario {usuario.nome} devolveu o livro {livro.titulo} com sucesso'
     }
+
+@biblioteca_router.get('/listar_emprestimos_usuario_logado')
+async def listar_usuario_logado(usuario: Usuarios = Depends(verificar_token), session: Session = Depends(pegar_sessao)):
+    emprestimos = session.query(Emprestimos).filter(Emprestimos.id_usuario==usuario.id).all()
+    if not emprestimos:
+        return {'message': 'Você não possui emprestimos ainda'}
+    return {
+        'message': 'Sucesso ao listar os seus emprestimos',
+        'emprestimos': [formatar_emprestimo(e) for e in emprestimos]
+    }
+
+@biblioteca_router.get('/listar_todos_emprestimos')
+async def listar_todos_emprestimos(usuario: Usuarios = Depends(verificar_admin), session: Session = Depends(pegar_sessao)):
+    emprestimos = session.query(Emprestimos).all()
+    return {
+        'message': 'Sucesso ao listar emprestimos',
+        'emprestimos': [formatar_emprestimo(e) for e in emprestimos]
+    }
+
+@biblioteca_router.get('/listar_emprestimos_ativos')
+async def listar_emprestimos_ativos(usuario: Usuarios = Depends(verificar_admin), session: Session = Depends(pegar_sessao)):
+    emprestimos = session.query(Emprestimos).filter(Emprestimos.status=='ativo').all()
+    if not emprestimos:
+        return {"message": "não existe nenhum emprestimo ativo agora"}
+    return {
+        'message': 'Sucesso ao proucurar emprestimos ativos',
+        'emprestimos_ativos': [formatar_emprestimo(e) for e in emprestimos]
+    }
+
+@biblioteca_router.get('/listar_emprestimos_de_alguem')
+async def listar_emprestimos_de_alguem(usuario_id: int, usuario: Usuarios = Depends(verificar_admin), session: Session = Depends(pegar_sessao)):
+    usuario_emprestimo = session.query(Usuarios).filter(Usuarios.id==usuario_id).first()
+    if not usuario_emprestimo:
+        raise HTTPException(status_code=404, detail='Usuário não existente')
+    emprestimos = session.query(Emprestimos).filter(Emprestimos.id_usuario==usuario_id).all()
+    if not emprestimos:
+        return {'message': f'O usuário {usuario_emprestimo.nome} não possui emprestimos'}
+    return {
+        'message': f'Sucesso ao buscar emprestimos de {usuario_emprestimo.nome}',
+        'emprestimos': [formatar_emprestimo(e) for e in emprestimos]
+    }
+
+@biblioteca_router.get('/listar_atrasados')
+async def listar_atrasados(session: Session = Depends(pegar_sessao), usuario: Usuarios = Depends(verificar_admin)):
+    session.query(Emprestimos).filter(
+    Emprestimos.status == "ativo",
+    Emprestimos.data_devolucao_prevista < datetime.utcnow()
+).update(
+    {"status": "atrasado"},
+    synchronize_session=False
+)
+    session.commit()
+    emprestimos = session.query(Emprestimos).filter(Emprestimos.status=='atrasado').all()
+    if not emprestimos:
+        return {'message': 'não possui nenhum emprestimo atrasado'}
+    return {
+        'emprestimos_atrasados': [formatar_emprestimo(e) for e in emprestimos]
+    }
+    
